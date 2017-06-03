994890c6d780b4b568a158830590d8b638b54a55
alex1rap <alex1rap@spaces.ru>
2017-06-03 23:50:06 +0300

    Добавлен файл настроек фреймворка

---
+++ config/web.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: alex1rap
+ * Date: 03.06.2017
+ * Time: 23:46
+ */
+
+return [
+    'language' => 'ru-RU',
+    'routing' => [
+        'errors' => [
+            'error404' => 'site/error404'
+        ],
+        'rules' => [
+            //
+        ]
+    ]
+];
