131fce57c7c29c0f4202b13df6a9b0d93368de65
alex1rap <alex1rap@spaces.ru>
2017-06-03 23:06:10 +0300

    Изменен индексный скрипт

---
+++ vendor/RAP/App.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: alex1rap
+ * Date: 03.06.2017
+ * Time: 22:59
+ */
+
+namespace RAP;
+
+class App
+{
+    public function __construct()
+    {
+    }
+
+    public function Run()
+    {
+        echo 'Runned';
+    }
+}
--- web/index.php
+++ web/index.php
@@ -6,3 +6,7 @@
  * Time: 22:46
  */
 
+require_once __DIR__ . '/../vendor/autoload.php';
+
+$app = new \RAP\App();
+$app->Run();
