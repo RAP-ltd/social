a50ceb5a1ff8e659e9908baf27b6fc6c5c3311db
alex1rap <alex1rap@spaces.ru>
2017-06-09 21:02:39 +0300

    Добавлен валидатор

--- views/calendar/index.php
+++ views/calendar/index.php
@@ -6,6 +6,8 @@
  * Time: 16:00
  */
 
+use RAP\helpers\Validator;
+
 $this->title = 'Calendar';
 
 // Вычисляем число дней в текущем месяце
@@ -17,14 +19,11 @@ $dayofmonth = date('t');
 $day_count = 1;
 
 
-
 // 1. Первая неделя
 
 $num = 0;
 
-for($i = 0; $i < 7; $i++)
-
-{
+for ($i = 0; $i < 7; $i++) {
 
     // Вычисляем номер дня недели для числа
 
@@ -36,13 +35,10 @@ for($i = 0; $i < 7; $i++)
 
     $dayofweek = $dayofweek - 1;
 
-    if($dayofweek == -1) $dayofweek = 6;
+    if ($dayofweek == -1) $dayofweek = 6;
 
 
-
-    if($dayofweek == $i)
-
-    {
+    if ($dayofweek == $i) {
 
         // Если дни недели совпадают,
 
@@ -54,11 +50,7 @@ for($i = 0; $i < 7; $i++)
 
         $day_count++;
 
-    }
-
-    else
-
-    {
+    } else {
 
         $week[$num][$i] = "";
 
@@ -67,18 +59,13 @@ for($i = 0; $i < 7; $i++)
 }
 
 
-
 // 2. Последующие недели месяца
 
-while(true)
-
-{
+while (true) {
 
     $num++;
 
-    for($i = 0; $i < 7; $i++)
-
-    {
+    for ($i = 0; $i < 7; $i++) {
 
         $week[$num][$i] = $day_count;
 
@@ -88,7 +75,7 @@ while(true)
 
         // из цикла
 
-        if($day_count > $dayofmonth) break;
+        if ($day_count > $dayofmonth) break;
 
     }
 
@@ -96,10 +83,9 @@ while(true)
 
     // из цикла
 
-    if($day_count > $dayofmonth) break;
+    if ($day_count > $dayofmonth) break;
 
 }
-
 
 
 // 3. Выводим содержимое массива $week
@@ -119,33 +105,25 @@ echo "<table border=1>
 <td style='color: red;'>Вс</td>
 </tr>";
 
-for($i = 0; $i < count($week); $i++)
-
-{
+for ($i = 0; $i < count($week); $i++) {
 
     echo "<tr>";
 
-    for($j = 0; $j < 7; $j++)
+    for ($j = 0; $j < 7; $j++) {
 
-    {
-
-        if(!empty($week[$i][$j]))
-
-        {
+        if (!empty($week[$i][$j])) {
 
             // Если имеем дело с субботой и воскресенья
 
             // подсвечиваем их
 
-            if($j == 5 || $j == 6)
+            if ($j == 5 || $j == 6)
 
-                echo "<a href=''<td><font color=red>".$week[$i][$j]."</font></td>";
+                echo "<td><a href='?day={$week[$i][$j]}'><span style='color:red;'>" . $week[$i][$j] . "</a></span></td>";
 
-            else echo "<td>".$week[$i][$j]."</td>";
+            else echo "<td><a href='?day={$week[$i][$j]}'>" . $week[$i][$j] . "</a></td>";
 
-        }
-
-        else echo "<td>&nbsp;</td>";
+        } else echo "<td>&nbsp;</td>";
 
     }
 
@@ -154,3 +132,49 @@ for($i = 0; $i < count($week); $i++)
 }
 
 echo "</table>";
+
+echo '<br>';
+
+$arr = [
+    '.',
+    ',',
+    '-',
+    '/'
+];
+
+$dates = [
+    [
+        20,
+        07,
+        1998
+    ],
+    [
+        30,
+        12,
+        1999
+    ],
+    [
+        32,
+        11,
+        2009
+    ],
+    [
+        22,
+        13,
+        2001
+    ],
+    [
+        11,
+        12,
+        2112
+    ],
+];
+
+foreach ($dates as $date) {
+    echo "<pre>";
+foreach ($arr as $dot) {
+    $dateString = "{$date[2]}{$dot}{$date[1]}{$dot}{$date[0]}";
+    echo "validate({$dateString} as date) = "; print_r(Validator::date($dateString)); echo "<br>";
+}
+    echo "</pre><br>";
+}
